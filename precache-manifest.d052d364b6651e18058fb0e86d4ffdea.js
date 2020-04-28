c66d49fa24414c1b3764a06de545d72595a855af
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-04-29 01:40:18 +0530

    Updates

---
+++ precache-manifest.d052d364b6651e18058fb0e86d4ffdea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbfce8a1d9de269f9310cac96f3e52c",
+    "url": "/DALIwebsite/index.html"
+  },
+  {
+    "revision": "8596e8de858bf81c6a68",
+    "url": "/DALIwebsite/static/css/main.12f688da.chunk.css"
+  },
+  {
+    "revision": "270e5ece9cb78ef9ab10",
+    "url": "/DALIwebsite/static/js/2.e458d1ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DALIwebsite/static/js/2.e458d1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8596e8de858bf81c6a68",
+    "url": "/DALIwebsite/static/js/main.eb4499a2.chunk.js"
+  },
+  {
+    "revision": "caf5c9a9cae8fd3006ca",
+    "url": "/DALIwebsite/static/js/runtime-main.44ba54e7.js"
+  },
+  {
+    "revision": "1949dfd925dd06c221ce6cea3d2a8d6b",
+    "url": "/DALIwebsite/static/media/Berk.1949dfd9.png"
+  },
+  {
+    "revision": "7651a98cdd8e21a6c8972476beeef28f",
+    "url": "/DALIwebsite/static/media/webdev.7651a98c.jpg"
+  }
+]);
