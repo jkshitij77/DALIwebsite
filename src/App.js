9047e002edfe55c7eae09b71195d8bb24b65e68a
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-05-16 14:52:31 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component{
               </div>
               <div class="needPadding"> 
                 <p class="alignCentre">
-                Hi, I am Kshitij, a '23 in Dartmouth from India. I am thinking of double majoring in math and CS owing to my passion for coding and love(read fear) of probability.
+                Hi, I am Kshitij, a Dart '23 from India. I am thinking of double majoring in math and CS owing to my passion for coding and love(read fear) of probability.
                 This website was coded to some sick lo-fi hip hop beats, like every other thing I have ever coded, things you can check out on my github by clicking below.
                 I absolutely love travelling and am going to live in a monastery for 2 weeks this summer (SO HYPED!).
                 If you are interested in seeing how it goes, keep up with this website cause this will soon be turning into a travel blog
@@ -48,15 +48,16 @@ export default class App extends React.Component{
             </div>
           </div>
         </div>
+
+        {/* What if I put all the different side components in route links so clicking on them gets me to a diff router link */}
+        {/* Do something on click? Something like routing or modals? */}
+        {/* Color scheme and content */}
+        {/* <SideComponent name="Anime"   url={"https://pm1.narvii.com/5797/d4564c452c878e60e2470b24b7a0d1dbcccccaad_hq.jpg"} divclass="odddiv flip-box" imageclass="oddimages" /> */}
         <div id="main">
-          {/* What if I put all the different side components in route links so clicking on them gets me to a diff router link */}
-          {/* Do something on click? Something like routing or modals? */}
-          {/* Color scheme and content */}
           <SideComponent name="WEBDEV"  url={require("./attachments/kshitijcertificate.png")} divclass="odddiv flip-box" imageclass="oddimages" />
           <SideComponent name="Unity"   url={"https://img-a.udemycdn.com/course/750x422/1210008_6859.jpg"} divclass="evendiv flip-box" imageclass="evenimages"/>
           <SideComponent name="Frisbee" url={"https://www.dartmouth.edu/~ultimate/img/WholeTeam.jpg"} divclass="odddiv flip-box" imageclass="oddimages"/>
           <SideComponent name="Travel"  url={require("./attachments/AmerFort.jpeg")} divclass="evendiv flip-box" imageclass="evenimages"/>
-          {/* <SideComponent name="Anime"   url={"https://pm1.narvii.com/5797/d4564c452c878e60e2470b24b7a0d1dbcccccaad_hq.jpg"} divclass="odddiv flip-box" imageclass="oddimages" /> */}
         </div>
       </div>
       </div>
--- src/SideCoponent.js
+++ src/SideCoponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 // import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-
+// If i need to do routing, I will need the commented out portions
 
 class SideComponent extends Component{
     constructor(props) {
@@ -40,36 +40,39 @@ class SideComponent extends Component{
             //             </Router>
             //         </div>
             // This kind of routing might also work. Where to route depends on what I click
-            heading=<div>
+            heading=<div class="lessWidth">
                         <h2>WebDev</h2>
-                        <p>This May 2020, I completed the WebDev Mini Course, the first of its kind offered by DALI Lab at Dartmouth <br />
-                        As a part of HTMLMAO, we found the need for a calendar that would make it easy to manage and find events held 
-                        by the Dartmouth Clubs<br />
-                        Have you checked it out yet? <br /><a href="https://dalicalendar-936c1.web.app" target="_blank" id="kegLink" rel="noopener noreferrer"><img class="kegPic" src={require('./attachments/wine.png')} alt="The Keg"/></a></p>
+                        <p class="para">This May 2020, I completed the WebDev Mini Course, the first of its kind offered by DALI Lab at Dartmouth <br />
+                        As a part of HTMLMAO, we found the need for a calendar that would make it easy to manage events held by the Dartmouth Clubs<br />
+                        Have you checked it out yet? 
+                        <br />
+                        <br />
+                        <a href="https://dalicalendar-936c1.web.app" target="_blank" id="kegLink" rel="noopener noreferrer"><img class="kegPic" src={require('./attachments/wine.png')} alt="The Keg"/></a></p>
                     </div>
         }
         else if(this.props.name==="Unity"){
-            heading=<div>
-                        <h2>Unity</h2>
-                        <p>I am extremely interested in Game development, especially AR and VR. <br /> 
-                            This summer, I am learning C# and Unity and working with a Dartmouth Professor and the ITC department <br />
-                            to find ways to implement VR experiences and make remote learning easier and more interactive for students</p>
-                    </div>
+            heading=
+            <div class="lessWidth">
+                <h1>Unity</h1>
+                <p class="para">I am extremely interested in Game development, especially AR and VR. <br /> 
+                    This summer, I am learning C# and Unity and working with a Dartmouth Professor and the ITC department <br />
+                    to find ways to implement VR experiences and make remote learning easier and more interactive for students</p>
+            </div>
         }
         else if(this.props.name==="Frisbee"){
             heading=
-            <div>
+            <div class="lessWidth">
                 <h2>Frisbee</h2>
-                <p>I tried frisbee for the first time in Dartmouth and absolutely fell in LOVE<br />
+                <p class="para">I tried frisbee for the first time in Dartmouth and absolutely fell in LOVE<br />
                 I have been a part of the disco trolls since fall and have found a solid communtiy out there
                 </p>
             </div>
         }
         else if(this.props.name==="Travel"){
             heading=
-            <div>
+            <div class="lessWidth">
                 <h2>Travel</h2>
-                <p>Can not get enough of of it. Like every other cliched teenager, How can I leave out Wanderlust<br />
+                <p class="para">Like every other person's cliched dream, how can I leave out travelling<br />
                 Plans to visit Kenya, Ethiopia, Zanzibar and South Africa in the winter. Catch it all on my blog</p>
             </div>
         }
